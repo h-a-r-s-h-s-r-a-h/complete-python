e8c08918d4e1ba1ea7b047e9524ecf39c26fc6d9
Harsh <harshvirat894@gmail.com>
2025-06-23 15:12:10 +0530

    importModulesAndPackages

---
+++ 05-importing-modules-packages/02-making-custom-packages/packages/subpackages/power.py
@@ -0,0 +1,3 @@
+def pow(a, b):
+    """Returns a raised to the power of b."""
+    return a**b
---
+++ 05-importing-modules-packages/02-making-custom-packages/test.py
@@ -0,0 +1,5 @@
+from packages import maths
+from packages.subpackages import power
+
+print(power.pow(2, 3))  # Should print 8
+print(maths.addition(2, 3))  # Should print 5
